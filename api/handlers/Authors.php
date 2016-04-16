176629b0040bfdb20c42dac888162363411f5228
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-04-16 02:04:43 -0500

    Authors API

---
+++ api/handlers/Authors.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace handlers;
+
+use lib\Error;
+use lib\Handler;
+
+class Authors extends Handler {
+
+    public function get($id) {
+
+        if (isset($id)) {
+            $response = $this->select(
+                'SELECT * FROM authors WHERE id = ?',
+                [$id]
+            );
+
+            if ($response) {
+                $response = $response[0];
+            } else {
+                (new Error('Author Not Found'))->send();
+            }
+
+            $response['books'] = (new Books())->getBooksByAuthor($id);
+        } else {
+            $response = $this->select(
+                'SELECT *, (SELECT count(*) FROM authorassoc WHERE authorid = a.id) as written ' .
+                'FROM authors a'
+            );
+        }
+
+        $this->send($response);
+    }
+
+    public function post() {
+
+        $this->send(func_get_args());
+    }
+}
